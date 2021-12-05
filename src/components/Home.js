5c94d860f2902a6aa040696e0ff7620f38cf0c3d
Paladín <test@test.com>
2021-12-05 14:06:47 -0500

    services table updated

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,41 @@
 import React, { Component } from "react";
+import Logo from '../logo.svg';
 
 export default class Home extends Component {
   render() {
+    function createServicesTable() {
+      const servicesTable = [];
+
+      for (let index = 0; index < 2; index++) {
+        const row = [];
+        for (let index = 0; index < 3; index++) {
+          row.push(
+            <td>
+              <div class="card is-flex is-flex-direction-column is-align-items-center">
+                <div class="card-image">
+                  <figure class="image is-128x128 is-flex">
+                    <img
+                      src={Logo}
+                      alt="Placeholder image"
+                    ></img>
+                  </figure>
+                </div>
+                <div class="card-content has-text-centered">
+                  <div className="title">Development</div>
+                  <div class="subtitle">
+                    Lorem ipsum leo risus, porta ac consectetur ac pite.
+                  </div>
+                </div>
+              </div>
+            </td>
+          );
+        }
+        servicesTable.push(<tr className="my-3">{row}</tr>);
+      }
+
+      return servicesTable;
+    }
+
     return (
       <div>
         <section className="hero is-info is-halfheight">
@@ -12,13 +46,16 @@ export default class Home extends Component {
             </div>
           </div>
         </section>
-        <section className="section is-flex is-flex-direction-column is-align-items-center">
-          <h1 className="title">Section</h1>
+        <section className="section has-text-centered">
+          <h1 className="title">Services</h1>
           <h2 className="subtitle">
-            A simple container to divide your page into{" "}
-            <strong>sections</strong>, like the one you're currently reading.
+            What do we have to offer? Check out all our services!
           </h2>
         </section>
+        <table className="table">
+          <thead></thead>
+          <tbody>{createServicesTable()}</tbody>
+        </table>
       </div>
     );
   }
