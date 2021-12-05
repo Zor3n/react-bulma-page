eb61c3a33af06e062d108f68888ff195fd8d31cf
Paladín <test@test.com>
2021-12-05 14:59:43 -0500

    who we are section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,34 @@
 import React, { Component } from "react";
-import Logo from '../logo.svg';
+import Logo from "../logo.svg";
 
 export default class Home extends Component {
   render() {
-    function createServicesTable() {
+    const keys = [
+      [1, 2, 3],
+      [4, 5, 6],
+    ];
+    const services = [
+      ["Development", "Account", "Security"],
+      ["Prom", "Pearl", "Ending"],
+    ];
+
+    const createServicesTable = () => {
       const servicesTable = [];
 
-      for (let index = 0; index < 2; index++) {
+      for (let indexR = 0; indexR < 2; indexR++) {
         const row = [];
-        for (let index = 0; index < 3; index++) {
+        for (let indexC = 0; indexC < 3; indexC++) {
           row.push(
-            <td>
-              <div class="card is-flex is-flex-direction-column is-align-items-center">
-                <div class="card-image">
-                  <figure class="image is-128x128 is-flex">
-                    <img
-                      src={Logo}
-                      alt="Placeholder image"
-                    ></img>
+            <td key={keys[indexR][indexC]}>
+              <div className="card is-flex is-flex-direction-column is-align-items-center">
+                <div className="card-image">
+                  <figure className="image is-128x128 is-flex">
+                    <img src={Logo} alt="React logo"></img>
                   </figure>
                 </div>
-                <div class="card-content has-text-centered">
-                  <div className="title">Development</div>
-                  <div class="subtitle">
+                <div className="card-content has-text-centered">
+                  <div className="title">{services[indexR][indexC]}</div>
+                  <div className="subtitle">
                     Lorem ipsum leo risus, porta ac consectetur ac pite.
                   </div>
                 </div>
@@ -30,25 +36,27 @@ export default class Home extends Component {
             </td>
           );
         }
-        servicesTable.push(<tr className="my-3">{row}</tr>);
+        servicesTable.push(
+          <tr key={indexR} className="my-3">
+            {row}
+          </tr>
+        );
       }
 
       return servicesTable;
-    }
+    };
 
     return (
       <div>
-        <section className="hero is-info is-halfheight">
+        <section className="hero is-medium is-info has-text-centered">
           <div className="hero-body">
-            <div className="">
-              <p className="title">Hero title home page :D</p>
-              <p className="subtitle">Hero subtitle</p>
-            </div>
+            <p className="title">Welcome to Parctuckings!</p>
+            <p className="subtitle">Take a look of all our services!</p>
           </div>
         </section>
         <section className="section has-text-centered">
-          <h1 className="title">Services</h1>
-          <h2 className="subtitle">
+          <h1 className="title is-size-3">Services</h1>
+          <h2 className="subtitle is-size-4">
             What do we have to offer? Check out all our services!
           </h2>
         </section>
@@ -56,6 +64,32 @@ export default class Home extends Component {
           <thead></thead>
           <tbody>{createServicesTable()}</tbody>
         </table>
+
+        <section className="section has-text-centered">
+          <h1 className="title is-size-3">Who we are</h1>
+          <h2 className="subtitle is-size-4">A little of our history!</h2>
+          <div className="box">
+            <div className="block is-size-5">
+              This text is within a <strong>second block</strong>. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit
+              amet massa fringilla egestas. Nullam condimentum luctus turpis.
+              This text is within a <strong>second block</strong>. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit
+              amet massa fringilla egestas. Nullam condimentum luctus turpis.
+            </div>
+            <div className="block is-size-5">
+              This text is within a <strong>second block</strong>. Lorem ipsum
+              dolor sit amet, consectetur adipiscing elit. Aenean efficitur sit
+              amet massa fringilla egestas. Nullam condimentum luctus turpis.
+            </div>
+          </div>
+        </section>
+
+        <section className="section has-text-centered">
+          <h1 className="title is-size-3">Messages</h1>
+          <h2 className="subtitle is-size-4">Read what our customers think!</h2>
+        </section>
+
       </div>
     );
   }
